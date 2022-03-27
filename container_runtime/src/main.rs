22bbef5c5db205945a4ee4ed885b885041e4390b
Kevin Per <kevin.per@protonmail.com>
2022-03-27 10:45:54 +0200

    Setup rust functions

--- container_runtime/src/main.rs
+++ container_runtime/src/main.rs
@@ -7,15 +7,53 @@ struct RunData {
 }
 
 fn main() {
-    let lua = Lua::new();
+    let mut lua = Lua::new();
 
     let data = RunData {
-        code: "return 1 + 2".to_string(),
+        code: r#"return stdlib(1)"#.to_string(),
     };
 
-    let x = lua.context(|lua_context| {
-       lua_context.load(&data.code).eval::<i32>().unwrap()
+    setup_game_library(&mut lua);
+
+    let x = lua.context(|lua_ctx| {
+        lua_ctx.load(&data.code).eval::<bool>().unwrap()
     });
 
     println!("{}", x);
+}
+
+/// Setup functions which the user can use directly in the lua code.
+fn setup_game_library(lua: &mut Lua) {
+
+    lua.context(|lua_ctx| {
+        let globals = lua_ctx.globals();
+        let check_equal = lua_ctx.create_function(|_, (list1, list2): (Vec<String>, Vec<String>)| {
+            // This function just checks whether two string lists are equal, and in an inefficient way.
+            // Lua callbacks return `rlua::Result`, an Ok value is a normal return, and an Err return
+            // turns into a Lua 'error'.  Again, any type that is convertible to Lua may be returned.
+            Ok(list1 == list2)
+        }).unwrap();
+
+        globals.set("check_equal", check_equal);
+
+        // ---
+
+        let check = lua_ctx.create_function(|_, (a, b): (usize, usize)| {
+            Ok(a == b)
+        }).unwrap();
+
+        globals.set("check", check);
+
+        // ---
+
+        let can_I_use_stdlib =  lua_ctx.create_function(|_, (a): (usize)| {
+            let mut h = std::collections::HashSet::new();
+            h.insert(1);
+            h.insert(2);
+            h.insert(3);
+            Ok(h.contains(a))
+        }).unwrap();
+
+        globals.set("stdlib", can_I_use_stdlib);
+    });
 }
